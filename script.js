aca3cc89f0ef096d7de697339a17b13e0d7bdbba
IsaPalandrani <143020084+IsaPalandrani@users.noreply.github.com>
2024-05-27 12:29:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,166 @@
+class Garota1{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garota2{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garota3{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garota4{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garota5{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garota6{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garota7{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garota8{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+
+class Garoto1{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garoto2{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garoto3{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garoto4{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garoto5{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garoto6{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garoto7{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+class Garoto8{
+    constructor(movimento, audicao, toque, idade, cor, genero, colaboracao, equilibrio, caracteristica, roupa, cabelo, olho, orelha, nariz, boca, altura, peso, personalidade, flexibilidade, forca, voz, mao, pe, cheiro, sabor, visao){
+
+    }
+}
+
+class Palco{
+    constructor(tamanho, altura, largura, profundidade, espaco, vazio, piso, teto, cortina, iluminacao, som, energia, camarim, escada, projecao){
+
+    }
+}
+
+class Música{
+    constructor(som, ritmo, sentimento, letra, batida, tempo, genero, melodia, harmonia, nota, timbre, instrumentos, vocal, acorde, banda){
+
+    }
+}
+
+class Lugar{
+    constructor(espaco, aberto, fechado, vazio, cheio, clima, pessoas, luz, publico, privado){
+
+    }
+}
+
+
+class Coreográfia{
+    constructor(passos, musica, figurino, ritmo, pessoas, sentimento, conexao, relacionamento){
+
+    }
+}
+
+class Dança{
+    constructor(ritmo, gingado, passos, coreografia, ensaio, tempo, molejo, estilo, energia, dancarinos){
+
+    }
+}
+
+class Ritmo{
+    constructor(tempo, musica, molejo, feling, harmonia, batida, compasso){
+
+    }
+}
+
+class Cadeiras{
+    constructor(assento, encosto, pernas, x, y, cor, pezinhos, material, estofado, tecido, desing){
+
+    }
+}
+
+class Luz{
+    constructor(){
+        
+    }
+}
+
+class Som{
+
+}
+
+class Janela{
+
+}
+
+class Ar{
+
+}
+
+class Cortina{
+
+}
+
+class Teto{
+
+}
+
+class Piso{
+
+}
+
+class Porta{
+
+}
+
+class Mesa{
+
+}
+
+class Celular{
+
+}
